5f7f020f49d025bc1a1560293fb7ea15db8157ad
Abdul Rahim <abdulrahim@techno.academy>
2025-07-20 23:06:43 +0530

    added computed filed and api orm decorator also added onchange in orm

--- custom_addons/real_estate_ads/models/property_offer.py
+++ custom_addons/real_estate_ads/models/property_offer.py
@@ -1,4 +1,6 @@
-from odoo import fields, models
+from reportlab.graphics.transform import inverse
+from datetime import timedelta
+from odoo import fields, models, api
 
 
 class PropertyOffer(models.Model):
@@ -16,4 +18,18 @@ class PropertyOffer(models.Model):
     partner_id = fields.Many2one('res.partner', string="Customer")
     property_id = fields.Many2one('estate.property', string="Property")
     validity = fields.Integer(string="Validity")
-    deadline = fields.Date(string="Deadline")
+    deadline = fields.Date(string='Deadline', compute='_compute_deadline', inverse='_inverse_deadline', store=True)
+    creation_date = fields.Date(string="Creation Date")
+
+    @api.depends('validity', 'creation_date')
+    def _compute_deadline(self):
+        """Compute the value of the field deadline."""
+        for record in self:
+            if record.creation_date and record.validity:
+                record.deadline = record.creation_date + timedelta(days=record.validity)  # Your computation here
+            else:
+                record.deadline = False
+
+    def _inverse_deadline(self):
+        for record in self:
+            record.validity = (record.deadline - record.creation_date).days
